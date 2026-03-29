18f1a139b635f0d249cb1e08a5713323df0823f3
riconuts <68708956+riconuts@users.noreply.github.com>
2026-03-29 05:17:10 -0300

    Delete Shader.hx
    Update Spork.hx

--- source/Spork.hx
+++ source/Spork.hx
@@ -61,6 +61,75 @@ class Spork {
 				return alpha = Value;
 			}
 		});
+
+		// catch shader errors and point to the error line in the shader code
+		Spoon.bend("openfl.display.Shader", macro class {
+			@:noCompletion private function __createGLProgram(vertexSource:String, fragmentSource:String):GLProgram
+			{
+				var gl = __context.gl;
+				var program = gl.createProgram();
+				try {
+					var vertexShader = __createGLShader(vertexSource, gl.VERTEX_SHADER);
+					var fragmentShader = __createGLShader(fragmentSource, gl.FRAGMENT_SHADER);
+
+					// Fix support for drivers that don't draw if attribute 0 is disabled
+					for (param in __paramFloat)
+					{
+						if (param.name.indexOf("Position") > -1 && StringTools.startsWith(param.name, "openfl_"))
+						{
+							gl.bindAttribLocation(program, 0, param.name);
+							break;
+						}
+					}
+
+					gl.attachShader(program, vertexShader);
+					gl.attachShader(program, fragmentShader);
+					gl.linkProgram(program);
+
+					if (gl.getProgramParameter(program, gl.LINK_STATUS) == 0)
+					{
+						var message = "Unable to initialize the shader program";
+						message += "\n" + gl.getProgramInfoLog(program);
+						Log.error(message);
+					}
+				}catch(e:Dynamic){
+					#if traceShaderLineNumbers 
+					if (e is String){
+						// sowy
+
+						var split:Array<String> = e.split('\n');
+
+						var errorLog:Array<String> = [];
+						var errorLines:Map<Int, Bool> = [];
+
+						for (_ in 0...split.indexOf('')){
+							var str = split.shift();
+							errorLog.push(str);
+
+							var parS = str.indexOf('(');
+							if (parS == -1) continue;
+
+							var parE = str.indexOf(')', parS);
+							var line:String = str.substr(parS + 1, parE - parS);
+							var lineVal = Std.parseInt(line);
+
+							if (lineVal != null)
+								errorLines.set(lineVal, true);
+						}
+
+						for (n in 1...split.length)
+							split[n] = (errorLines.exists(n) ? 'Error here ->' : '($n)') + split[n];
+
+						e =	split.join('\n') + '\n\n' + errorLog.join('\n');
+					}
+					#end
+					
+					trace(e);
+				}
+
+				return program;
+			}
+		});
 		#end
 	}
 }
